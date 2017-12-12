c0ef5c407fecd55a6dae83ff0bdb1334ba347153
momo5502 <mail@momo5502.com>
2017-12-12 18:21:30 +0100

    Fix 32bit compilation

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ void parse_oui_data()
 	std::string str;
 
 	t.seekg(0, std::ios::end);
-	str.reserve(t.tellg());
+	str.reserve(size_t(t.tellg()));
 	t.seekg(0, std::ios::beg);
 
 	str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
